b6a61e930de4e1869a8bff0d63a055409b0e1522
greekik <greekik@gmail.com>
2019-02-04 12:26:23 +0300

    01

--- app/src/main/java/com/example/smarthouse/MainActivity.java
+++ app/src/main/java/com/example/smarthouse/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Button sendOne = (Button) findViewById(R.id.button)
     }
 
     @Override
