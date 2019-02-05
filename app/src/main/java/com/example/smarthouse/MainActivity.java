ddf0246321db4035898e063e652f5a3694204350
greekik <47242277+greekik@users.noreply.github.com>
2019-02-05 11:48:29 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/smarthouse/MainActivity.java
+++ app/src/main/java/com/example/smarthouse/MainActivity.java
@@ -32,14 +32,34 @@ public class MainActivity extends AppCompatActivity {
         Button butSendNull = (Button) findViewById(R.id.buttonOff);
         Button butConnect = (Button) findViewById(R.id.buttonOff);
         
-        OnClickListener listenerOfsendOne = new OnClickListener() {
+        OnClickListener listenerOfbutConnect = new OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                private void runTcpClient() {
+                    try {
+                        Socket s = new Socket("localhost", TCP_SERVER_PORT);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+                        //send output msg
+                        String outMsg = "TCP connecting to " + TCP_SERVER_PORT + System.getProperty("line.separator"); 
+                        out.write(outMsg);
+                        out.flush();
+                        Log.i("TcpClient", "sent: " + outMsg);
+                        //accept server response
+                        String inMsg = in.readLine() + System.getProperty("line.separator");
+                        Log.i("TcpClient", "received: " + inMsg);
+                        //close connection
+                        s.close();
+                    } catch (UnknownHostException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } 
+                }
             }
         };
  
-        butSendOne.setOnClickListener(listenerOfsendOne);
+        butConnect.setOnClickListener(listenerOfbutConnect);
     }
 
     @Override
