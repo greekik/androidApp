457551bc3523750f6041aa58211fd95cce80f284
greekik <47242277+greekik@users.noreply.github.com>
2019-02-05 14:11:55 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/smarthouse/MainActivity.java
+++ app/src/main/java/com/example/smarthouse/MainActivity.java
@@ -25,44 +25,44 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                    .setAction("Action", null).show();
             }
         });
         Button butSendOne = (Button) findViewById(R.id.buttonOn);
         Button butSendNull = (Button) findViewById(R.id.buttonOff);
         Button butConnect = (Button) findViewById(R.id.butConnect);
-        
+
         OnClickListener listenerOfbutConnect = new OnClickListener() {
             public void onClick(View v) {
-				switch (v.getId()){
-					case R.id.butConnect:
-						private void runTcpClient() {
-							ServerSocket ss = null;
-							try {
-								Socket s = new Socket("myhomerouter.ddns.net", 9090);
-								BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-								BufferedWriter out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
-								//send output msg
-								String outMsg = "TCP connecting to " + "9090" + System.getProperty("line.separator"); 
-								out.write(outMsg);
-								out.flush();
-								Log.i("TcpClient", "sent: " + outMsg);
-								//accept server response
-								String inMsg = in.readLine() + System.getProperty("line.separator");
-								Log.i("TcpClient", "received: " + inMsg);
-								//close connection
-								s.close();
-							} catch (UnknownHostException e) {
-								e.printStackTrace();
-							} catch (IOException e) {
-								e.printStackTrace();
-							} 
-						}
-					break;
-				}
+                switch (v.getId()) {
+                    case R.id.butConnect:
+                        private void runTcpClient() {
+                            ServerSocket ss = null;
+                            try {
+                                Socket s = new Socket("myhomerouter.ddns.net", 9090);
+                                BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+                                //send output msg
+                                String outMsg = "TCP connecting to " + "9090" + System.getProperty("line.separator");
+                                out.write(outMsg);
+                                out.flush();
+                                Log.i("TcpClient", "sent: " + outMsg);
+                                //accept server response
+                                String inMsg = in .readLine() + System.getProperty("line.separator");
+                                Log.i("TcpClient", "received: " + inMsg);
+                                //close connection
+                                s.close();
+                            } catch (UnknownHostException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        break;
+                }
             }
         };
- 
+
         butConnect.setOnClickListener(listenerOfbutConnect);
     }
 
