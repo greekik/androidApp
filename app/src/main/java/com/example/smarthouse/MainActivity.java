9cb3016d049af2099d94a0b15b0096a95e64c35c
greekik <47242277+greekik@users.noreply.github.com>
2019-02-08 14:16:28 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/smarthouse/MainActivity.java
+++ app/src/main/java/com/example/smarthouse/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.smarthouse;
 
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.os.Message
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -51,8 +52,12 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
-
         butConnect.setOnClickListener(listenerOfbutConnect);
+        
+        public void handleMessage(Message msg) {
+            // обновляем TextView
+            butConnect.setText((String) msg);
+        };
     }
 
     @Override
