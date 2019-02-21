482b2ed02a8f14380a2a3f2a4e345d76ac7d40ff
greekik <47242277+greekik@users.noreply.github.com>
2019-02-21 17:01:54 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/smarthouse/MainActivity.java
+++ app/src/main/java/com/example/smarthouse/MainActivity.java
@@ -122,6 +122,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
